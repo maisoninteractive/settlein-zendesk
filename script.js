e9ddbdf285525fffc6818ebb826bab543f40a390
Maisoninteractive <tech@maisoninteractive.agency>
2022-03-27 01:16:50 +0300

    Fix:checking image url for extension

--- script.js
+++ script.js
@@ -494,11 +494,12 @@ document.addEventListener('DOMContentLoaded', function() {
                         $('article-wrap-' + element['id']).css({ 'padding-lef': '25px' });
                     } else {
                         let urlimg = imageUrl.article_attachments[0].content_url;
-                        if (!self.checkURL(urlimg) || (typeof urlimg === 'undefined')) {
+                        if (self.checkURL(urlimg)) {
+                            $('#article-' + element['id']).attr('src', imageUrl.article_attachments[0].content_url);
+
+                        } else {
                             $('img-wrap-' + element['id']).remove();
                             $('article-wrap-' + element['id']).css({ 'padding-left': '25px' });
-                        } else {
-                            $('#article-' + element['id']).attr('src', imageUrl.article_attachments[0].content_url);
                         }
                     }
                 });
